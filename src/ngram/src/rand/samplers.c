b940279eba423c30e7722e320ec4a10f8a87ce2b
wrathematics <wrathematics@gmail.com>
2014-06-09 00:49:36 -0400

    fixed seed issue with sample()

--- src/ngram/src/rand/samplers.c
+++ src/ngram/src/rand/samplers.c
@@ -14,7 +14,7 @@ int sample(rng_state_t *rs, const int min, const int max)
   int ret;
   
   // Check that the RNG has been initialized; if not, start it
-  rng_check(rs);
+/*  rng_check(rs);*/
   
   ret = min + (int) ((double)(max-min + 1) * rng_extract(rs)*I32BIT);
   
