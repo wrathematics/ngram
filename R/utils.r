f55112b9941fd45a0c619379e856a171213adddb
wrathematics <wrathematics@gmail.com>
2014-06-21 17:44:54 -0400

    fixed concat, added preprocess

--- R/utils.r
+++ R/utils.r
@@ -1,12 +1,37 @@
-concat <- function(str, collapse="", rm.space=FALSE)
+concat <- function(..., collapse="", rm.space=FALSE)
 {
-  if (length(str) > 1)
-    str <- paste(sapply(str, concat), collapse=collapse)
+  l <- list(...)
+  
+  str <- paste(sapply(l, paste, collapse=collapse), collapse=collapse)
   
   if (rm.space)
     str <- gsub(x=str, pattern=" ", replacement="")
   
   return( str )
+}
+
+
+
+preprocess <- function(x, case=NULL, split.at.punct=FALSE)
+{
+  if (!is.null(case))
+  {
+    case <- match.arg(tolower(case), c("lower", "upper"))
+    if (case == "lower")
+      x <- tolower(x)
+    else if (case == "upper") 
+      x <- toupper(x)
+  }
+  
+  if (split.at.punct)
+    x <- gsub(x=x, pattern="([[:punct:]])", replacement=" \\1 ")
+  
+  # fix spacing
+  x <- gsub(pattern="(\\t| +|[\\h\\v])", replacement=" ", x=x)
+  # remove trailing whitespace
+  x <- sub(pattern=" +$", replacement="", x=x)
+  
+  return( x )
 }
 
 
