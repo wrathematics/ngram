787ff962d8b6ccdb3440f17cc2a0ddf9d305b23c
wrathematics <wrathematics@gmail.com>
2014-06-21 22:09:00 -0400

    hiding rgutenberg methods

--- R/rgutenberg.r
+++ R/rgutenberg.r
@@ -1,17 +1,17 @@
-setMethod("ngram", signature(x="book"),
-  function(x, n=2)
-  {
-    str <- rgutenberg::text(words=x, with.license=FALSE)
-    
-    return( ngram(x=str, n=n) )
-  }
-)
+#setMethod("ngram", signature(x="book"),
+#  function(x, n=2)
+#  {
+#    str <- rgutenberg::text(words=x, with.license=FALSE)
+#    
+#    return( ngram(x=str, n=n) )
+#  }
+#)
 
-setMethod("wordcount", signature(x="book"),
-  function(x, n=2)
-  {
-    str <- rgutenberg::text(words=x)
-    
-    return( ngram(x=str, n=n) )
-  }
-)
+#setMethod("wordcount", signature(x="book"),
+#  function(x, n=2)
+#  {
+#    str <- rgutenberg::text(words=x)
+#    
+#    return( ngram(x=str, n=n) )
+#  }
+#)
