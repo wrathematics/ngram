6c73f5cfb9fac8814dc09e681a6cd955e2fca97a
wrathematics <wrathematics@gmail.com>
2014-12-03 22:21:21 -0500

    added license header to library source

--- src/ngram/src/rand/rng_interface.h
+++ src/ngram/src/rand/rng_interface.h
@@ -1,5 +1,32 @@
-#ifndef SHUFFLER_RNG_INTERFACE_H
-#define SHUFFLER_RNG_INTERFACE_H
+/*  Copyright (c) 2013, Heckendorf
+    All rights reserved.
+    
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+    
+    1. Redistributions of source code must retain the above copyright notice,
+    this list of conditions and the following disclaimer.
+    
+    2. Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in the
+    documentation and/or other materials provided with the distribution.
+    
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+    TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+    PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+    CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+    EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+    PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+    PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+    LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+
+#ifndef NGRAM_RNG_INTERFACE_H
+#define RAND_RNG_INTERFACE_H
 
 #include <stdint.h>
 
@@ -34,5 +61,6 @@ void rng_check(rng_state_t *);
 void rng_init(rng_state_t *, const uint32_t seed);
 uint32_t rng_extract(rng_state_t *);
 void rng_free(rng_state_t *rs);
+
 
 #endif
