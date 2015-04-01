44996380692004ab4280b6fece9800d954397ccc
wrathematics <wrathematics@gmail.com>
2015-04-01 19:25:10 -0400

    fixed tests

--- tests/test.r
+++ tests/test.r
@@ -6,4 +6,4 @@ ng <- ngram(x)
 test <- babble(ng, seed=1234)
 truth <- "b a c a b a c a b b c a b a c a b b b b c a b b a c a b b b a c a b a c a b a c a b b b b b b b a c a b a c a b b a b b b b a b b b a c a b a c a b a c a b b b a c a b b b b b b b b c a b a c a b b a b a c a b b b a c a b b a c a b a c a b b a c a b a c a b a c a b a c a b a c a b b a b a c a b a c "
 
-all.equal(test, truth)
+stopifnot(all.equal(test, truth))
