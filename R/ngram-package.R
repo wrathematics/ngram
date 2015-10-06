110eee97f138fdc5c2f317753bedaf1d058b3c3e
wrathematics <wrathematics@gmail.com>
2015-10-06 10:13:51 -0400

    changed package title, description

--- R/ngram-package.R
+++ R/ngram-package.R
@@ -1,7 +1,11 @@
 #' ngram: An n-gram Babbler
 #' 
-#' This package offers utilities for creating, displaying, and "babbling"
-#' n-grams.  The babbler is a simple Markov process.
+#' An n-gram is a sequence of n "words" taken from a
+#' body of text.  This package offers utilities for creating, 
+#' displaying, summarizing, and "babbling" n-grams.  The 
+#' tokenization and "babbling" are handled by very efficient C
+#' code, which can even be build as its own standalone library.
+#' The babbler is a simple Markov chain.
 #' 
 #' The ngram package is distributed under the permissive 2-clause BSD license.
 #' If you find the code here useful, please let us know and/or cite the
