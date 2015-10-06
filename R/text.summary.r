a0abf333fb25da224dd8f9846f81abaa982e1a27
wrathematics <wrathematics@gmail.com>
2015-10-06 11:19:02 -0400

    added string.summary()

---
+++ R/text.summary.r
@@ -0,0 +1,83 @@
+title_case <- function(x) gsub(x, pattern="(^|[[:space:]])([[:alpha:]])", replacement="\\1\\U\\2", perl=TRUE)
+
+
+
+#' Text Summary
+#' 
+#' @param string
+#' An input string.
+#' @param wordlen_max,senlen_max,syllen_max
+#' The maximum lengths of words/sentences/syllables to consider.
+#' 
+#' @return
+#' A list of class \code{string_summary}.
+#' 
+#' @examples
+#' x <- "a b a c a b b"
+#' 
+#' string.summary(x)
+#' 
+#' @export
+string.summary <- function(string, wordlen_max=10, senlen_max=10, syllen_max=10)
+{
+  if (length(string) > 1)
+    warning("Argument 'string' has length > 1 and only the first element will be used.")
+  
+  wordlen_max <- as.integer(wordlen_max)
+  if (is.na(wordlen_max) || wordlen_max < 1)
+    stop("Argument 'wordlen_max' must be a positive integer.")
+  
+  senlen_max <- as.integer(senlen_max)
+  if (is.na(senlen_max) || senlen_max < 1)
+    stop("Argument 'senlen_max' must be a positive integer.")
+  
+  syllen_max <- as.integer(syllen_max)
+  if (is.na(syllen_max) || syllen_max < 1)
+    stop("Argument 'syllen_max' must be a positive integer.")
+  
+  
+  ret <- .Call(R_stringsummary, string, wordlen_max, senlen_max, syllen_max)
+  class(ret) <- "string_summary"
+  
+  ret
+}
+
+
+
+spacenames <- function(x)
+{
+  maxlen <- max(sapply(names(x), nchar))
+  names <- title_case(x=names(x))
+  spacenames <- simplify2array(lapply(names, function(str) paste0(str, ":", paste0(rep(" ", maxlen-nchar(str)), collapse=""))))
+}
+
+
+
+#' @method print string_summary
+#' @export
+print.string_summary <- function(x, ...)
+{
+  sn <- spacenames(x)
+  
+  len <- length(x)
+  indx <- (len-2):len
+  y <- x[indx]
+  x <- x[-indx]
+  
+  sn_x <- sn[-indx]
+  cat(paste(sn_x, x, sep=" ", collapse="\n"), "\n")
+  
+  sn_y <- sn[indx]
+  for (i in 1:length(y))
+  {
+    tab <- table(y[i])
+    names <- names(tab)
+    dimnames(tab) <- NULL
+#    cat(sn_y[i])
+    cat(paste(sn_y[i], paste0(names, collapse=" "), sep=" ", collapse="\n"), "\n")
+    cat(paste(paste(rep(" ", nchar(sn_y[1])), collapse=""), paste0(tab, collapse=" "), sep=" ", collapse="\n"), "\n")
+  }
+  
+  invisible()
+}
+
--- src/count.c
+++ src/count.c
@@ -41,64 +41,213 @@ SEXP R_ngram_wordcount(SEXP str, SEXP sep)
 
 
 
-
+#define INT(x) INTEGER(x)[0]
+#define CHARPT(x,i) ((char*)CHAR(STRING_ELT(x,i)))
 #define LEN1INTVEC(Rname,value) \
   PROTECT(Rname = allocVector(INTSXP, 1)); \
   INTEGER(Rname)[0] = value
 
 #define MIN(a,b) (a<b?a:b)
 
-SEXP R_stringsummary(SEXP string, SEXP wordlen_max)
+#include <R.h>
+#include <Rinternals.h>
+
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+
+// TODO should we worry about funny euro vowels?
+static inline bool isvowel(const char c)
 {
+  if (c == 'a' || c == 'A' || c == 'e' || c == 'E' || c == 'i' || c == 'I' ||
+      c == 'o' || c == 'O' || c == 'u' || c == 'U' || c == 'y' || c == 'Y')
+    return true;
+  else
+    return false;
+}
+
+
+
+SEXP R_stringsummary(SEXP string, SEXP wordlen_max_, SEXP senlen_max_, SEXP syllen_max_)
+{
+  int chars = 0, letters = 0, whitespace = 0, punctuation = 0, digits = 0;
+  int words = 0, sentences = 0, lines = 0;
+  const int wordlen_max = INT(wordlen_max_);
+  const int senlen_max = INT(senlen_max_);
+  const int syllen_max = INT(syllen_max_);
+  int i, j, tmp;
+  char c, *str;
+  bool multispace_correction;
+  
   SEXP ret, ret_names;
   SEXP Rchars, Rletters, Rwhitespace, Rpunctuation, Rdigits;
   SEXP Rwords, Rsentences, Rlines;
   
   SEXP wordlens;
-  PROTECT(wordlens = allocVector(INTSXP, INT(wordlen_max)));
+  PROTECT(wordlens = allocVector(INTSXP, wordlen_max));
+  for (i=0; i<wordlen_max; i++)
+    INTEGER(wordlens)[i] = 0;
   
-  ngram_summary_t ngsum;
-  ngram_stringsummary(CHARPT(string, 0), INT(wordlen_max), &ngsum);
+  SEXP senlens;
+  PROTECT(senlens = allocVector(INTSXP, senlen_max));
+  for (i=0; i<senlen_max; i++)
+    INTEGER(senlens)[i] = 0;
   
-  for (int i=0; i<INT(wordlen_max); i++)
-    INTEGER(wordlens)[i] = ngsum.wordlens[i];
+  SEXP syllens;
+  PROTECT(syllens = allocVector(INTSXP, syllen_max));
+  for (i=0; i<syllen_max; i++)
+    INTEGER(syllens)[i] = 0;
   
-  LEN1INTVEC(Rchars, ngsum.chars);
-  LEN1INTVEC(Rletters, ngsum.letters);
-  LEN1INTVEC(Rwhitespace, ngsum.whitespace);
-  LEN1INTVEC(Rpunctuation, ngsum.punctuation);
-  LEN1INTVEC(Rdigits, ngsum.digits);
-  LEN1INTVEC(Rwords, ngsum.words);
-  LEN1INTVEC(Rsentences, ngsum.sentences);
-  LEN1INTVEC(Rlines, ngsum.lines);
+  const int lenstr = LENGTH(string);
   
-  PROTECT(ret = allocVector(VECSXP, 9));
-  SET_VECTOR_ELT(ret, 0, Rchars);
-  SET_VECTOR_ELT(ret, 1, Rletters);
-  SET_VECTOR_ELT(ret, 2, Rwhitespace);
-  SET_VECTOR_ELT(ret, 3, Rpunctuation);
-  SET_VECTOR_ELT(ret, 4, Rdigits);
-  SET_VECTOR_ELT(ret, 5, Rwords);
-  SET_VECTOR_ELT(ret, 6, Rsentences);
-  SET_VECTOR_ELT(ret, 7, Rlines);
-  SET_VECTOR_ELT(ret, 8, wordlens);
+  for (j=0; j<lenstr; j++)
+  {
+    i = 0;
+    int wordlen_current =  0, senlen_current =  0, syllen_current = 0;
+    str = CHARPT(string, j);
+    
+    lines++;
+    
+    while ((c=str[i]) != '\0')
+    {
+      chars++;
+      
+      multispace_correction = false;
+      
+      // new word
+      if (isspace(c))
+      {
+        if (c != '\n')
+          whitespace++;
+        else
+          lines++;
+        
+        words++;
+        
+        INTEGER(wordlens)[MIN(wordlen_current, wordlen_max)-1]++;
+        wordlen_current =  0;
+        
+        senlen_current++;
+        
+        INTEGER(syllens)[MIN(syllen_current, syllen_max)-1]++;
+        syllen_current =  0;
+      }
+      else
+      {
+        wordlen_current++;
+        
+        if (ispunct(c))
+        {
+          punctuation++;
+          if (c=='.' || c==';' || c=='!' || c=='?')
+          {
+            sentences++;
+            INTEGER(senlens)[MIN(senlen_current, senlen_max)-1]++;
+            senlen_current =  0;
+          }
+        }
+        else
+        {
+          if (isalpha(c))
+          {
+            letters++;
+            
+            if (isvowel(c) && !isvowel(str[i-1]))
+              syllen_current++;
+          }
+          else if (isdigit(c))
+            digits++;
+        }
+      }
+      
+      // skip multiple spaces
+      do
+      {
+        if (isspace(c))
+        {
+          chars++;
+          whitespace++;
+          multispace_correction = true;
+          i++;
+        }
+        else
+          break;
+      } while ((c=str[i]) != '\0');
+      
+      if (!multispace_correction)
+        i++;
+      else
+      {
+        chars--;
+        if (c != '\n')
+          whitespace--;
+      }
+    }
+    
+    // Count words that end lines unless they end with a trailing hyphen
+    if (i == 0) 
+      continue;
+    
+    c = str[i-1];
+    if (!isspace(c) && c!='-')
+    {
+      words++;
+      INTEGER(wordlens)[MIN(wordlen_current, wordlen_max)-1]++;
+      
+      INTEGER(syllens)[MIN(syllen_current, syllen_max)-1]--;
+    }
+    else if (c=='-' && j<lenstr-1)
+    {
+      words--;
+      INTEGER(wordlens)[MIN(wordlen_current, wordlen_max)-1]--;
+      
+      INTEGER(syllens)[MIN(syllen_current, syllen_max)-1]--;
+      
+      senlen_current--;
+    }
+  }
   
-  PROTECT(ret_names = allocVector(STRSXP, 9));
-  SET_STRING_ELT(ret_names, 0, mkChar("chars"));
-  SET_STRING_ELT(ret_names, 1, mkChar("letters"));
-  SET_STRING_ELT(ret_names, 2, mkChar("whitespace"));
-  SET_STRING_ELT(ret_names, 3, mkChar("punctuation"));
-  SET_STRING_ELT(ret_names, 4, mkChar("digits"));
-  SET_STRING_ELT(ret_names, 5, mkChar("words"));
-  SET_STRING_ELT(ret_names, 6, mkChar("sentences"));
-  SET_STRING_ELT(ret_names, 7, mkChar("lines"));
-  SET_STRING_ELT(ret_names, 8, mkChar("wordlens"));
+  
+  LEN1INTVEC(Rchars, chars);
+  LEN1INTVEC(Rletters, letters);
+  LEN1INTVEC(Rwhitespace, whitespace);
+  LEN1INTVEC(Rpunctuation, punctuation);
+  LEN1INTVEC(Rdigits, digits);
+  LEN1INTVEC(Rwords, words);
+  LEN1INTVEC(Rsentences, sentences);
+  LEN1INTVEC(Rlines, lines);
+  
+  PROTECT(ret = allocVector(VECSXP, 11));
+  SET_VECTOR_ELT(ret,  0, Rchars);
+  SET_VECTOR_ELT(ret,  1, Rletters);
+  SET_VECTOR_ELT(ret,  2, Rwhitespace);
+  SET_VECTOR_ELT(ret,  3, Rpunctuation);
+  SET_VECTOR_ELT(ret,  4, Rdigits);
+  SET_VECTOR_ELT(ret,  5, Rwords);
+  SET_VECTOR_ELT(ret,  6, Rsentences);
+  SET_VECTOR_ELT(ret,  7, Rlines);
+  SET_VECTOR_ELT(ret,  8, wordlens);
+  SET_VECTOR_ELT(ret,  9, senlens);
+  SET_VECTOR_ELT(ret, 10, syllens);
+  
+  PROTECT(ret_names = allocVector(STRSXP, 11));
+  SET_STRING_ELT(ret_names,  0, mkChar("chars"));
+  SET_STRING_ELT(ret_names,  1, mkChar("letters"));
+  SET_STRING_ELT(ret_names,  2, mkChar("whitespace"));
+  SET_STRING_ELT(ret_names,  3, mkChar("punctuation"));
+  SET_STRING_ELT(ret_names,  4, mkChar("digits"));
+  SET_STRING_ELT(ret_names,  5, mkChar("words"));
+  SET_STRING_ELT(ret_names,  6, mkChar("sentences"));
+  SET_STRING_ELT(ret_names,  7, mkChar("lines"));
+  SET_STRING_ELT(ret_names,  8, mkChar("wordlens"));
+  SET_STRING_ELT(ret_names,  9, mkChar("senlens"));
+  SET_STRING_ELT(ret_names, 10, mkChar("syllens"));
   setAttrib(ret, R_NamesSymbol, ret_names);
   
-  free(ngsum.wordlens);
   
-  UNPROTECT(11);
+  UNPROTECT(13);
   return ret;
 }
-
 
