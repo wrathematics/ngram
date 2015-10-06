149ce48de6bd855404a43b79158f29f1e9622da2
wrathematics <wrathematics@gmail.com>
2015-10-06 10:14:34 -0400

    added random corpus

---
+++ R/rcorpus.r
@@ -0,0 +1,28 @@
+#' Random Corpus
+#' 
+#' Generate a corpus of random "words".
+#' 
+#' @param nwords
+#' Number of words to generate.
+#' @param minwordlen,maxwordlen
+#' The min/max length of words in the generated corpus.
+#' 
+#' @return
+#' A string.
+#' 
+#' @examples
+#' rcorpus(10)
+#' 
+#' @export
+rcorpus <- function(nwords, minwordlen=1, maxwordlen=6)
+{
+  if (minwordlen < 1)
+    stop("Argument 'minwordlen' must be at least 1.")
+  if (minwordlen > maxwordlen)
+    stop("Argument 'maxwordlen' must be at least 'minwordlen'.")
+  
+  sizes <- as.integer(runif(nwords, minwordlen, maxwordlen+1))
+  words <- sapply(sizes, function(size) paste0(sample(letters, size=size, replace=TRUE), collapse=""))
+  
+  paste0(words, collapse=" ")
+}
