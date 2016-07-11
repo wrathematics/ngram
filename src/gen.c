1245695d03ba3b8e74fa71ca1a00e1ea7c36ed91
Drew Schmidt <wrathematics@gmail.com>
2016-07-11 14:13:44 -0400

    fix mem leaks in babbler

--- src/gen.c
+++ src/gen.c
@@ -41,13 +41,15 @@ SEXP R_ngram_gen(SEXP ngl_ptr, SEXP genlen, SEXP seed)
   rng_init(&rs, INT(seed));
   
   retlen = ngram_gen(&rs, ngl, INT(genlen), &ret);
-  if(retlen < 0){
-    rng_free(&rs);
+  
+  rng_free(&rs);
+  
+  if (retlen < 0)
     error("allocation error");
-  }
   
   PROTECT(RET = allocVector(STRSXP, 1));
   SET_STRING_ELT(RET, 0, mkCharLen(ret, retlen));
+  free(ret);
   
   UNPROTECT(1);
   return RET;
